913ea3da06b6c393a20bb5068bd1c7feb3371700
Jackspence6 <jackspence123456@gmail.com>
2023-07-07 10:27:56 +0200

    Added Function with loop to change row highlight color depending on current hour.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,12 @@
 /* External dependencies */
 /******************************************/
 var now = $("#currentDay");
+var tableHour = $(".time");
 
 /******************************************/
 /* Global variables and constants */
 /******************************************/
-
+var hourNow;
 /******************************************/
 /* Function and class declarations */
 /******************************************/
@@ -15,6 +16,20 @@ function dateDisplay() {
   var rightNow = moment().format("dddd, MMMM Do YYYY, h:mm:ss a");
   now.text(rightNow);
 }
+
+// Function with loop to change row highlight color depending on current hour
+function rowColor() {
+  hourNow = moment().hour();
+  $("tr").each(function () {
+    var rowHour = parseInt($(this).find(".time").text());
+    console.log(rowHour);
+
+    if (rowHour === hourNow) {
+      $(this).css("background-color", "red");
+    }
+  });
+}
+rowColor();
 
 /******************************************/
 /* Event listeners */
